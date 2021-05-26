edf11d8b959bdafbca7900b9b61882ae67b1f226
Mark Hanna <cipscis+github@gmail.com>
2021-05-26 18:02:17 +1200

    v1.0.1 - Moved docs into main

---
+++ docs/assets/js/src/main.js
@@ -0,0 +1,16 @@
+import throttle from 'throttle';
+import activate from 'activate';
+
+const increment = function (e) {
+	e.preventDefault();
+
+	let el = e.target;
+	let activateNum = parseInt(el.getAttribute('data-activate-count'));
+
+	activateNum += 1;
+
+	el.setAttribute('data-activate-count', activateNum);
+};
+
+activate('.js-throttle-fast', throttle(increment, 200));
+activate('.js-throttle-slow', throttle(increment, 1000));
